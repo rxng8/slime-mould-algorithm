f313a17eb472f2437e69f76385434540ad634842
Faezeh Habibi <155960330+Faezehabibi@users.noreply.github.com>
2024-03-15 12:03:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,53 @@
+# I coppied Ugly-Viet's code. Copy Right did not preserved ha ha.
 
-# %%
 
 from typing import List, Dict, Tuple, Callable, Sequence, Any
 import numpy as np
 
 from lib import Config
 from lib.types import MetricsType
+from lib.benchmarks import Ackley, Rosenbrock, negative_Alpine
 from lib.algorithms import SlimeMould
 
+# initial config
 config = Config(
-  pop_size = 100,
-  dim = 2,
-  initializer = "normal"
+  pop_size = 1000,
+  dim = 10,
+  max_iters = 20,
+  minimizing = True,
+  seed = 69
 )
-config = config.update(pop_size=50)
+
+np.random.seed(config.seed)
+
+####### Run Ackley #####
+print("\nAckley:")
+fn = Ackley
+config = config.update(minimizing=fn.minimizing)
 print(config)
+S = SlimeMould(fn, config)
+S.run()
 
+####### Run Rosenbrock #####
+print("\nRosenbrock:")
+fn = Rosenbrock
+config = config.update(
+  minimizing=fn.minimizing,
+  max_iters=100,
+  dim=2
+)
+print(config)
+S = SlimeMould(fn, config)
+S.run()
 
-S = SlimeMould(config)
-
-# to make sure this code is safe  ha ha
+####### Run Rosenbrock #####
+print("\nnegative_Alpine")
+fn = negative_Alpine
+config = config.update(
+  minimizing=fn.minimizing,
+  max_iters=20,
+  dim=10
+)
+print(config)
+S = SlimeMould(fn, config)
+S.run()
