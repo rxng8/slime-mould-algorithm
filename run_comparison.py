1e2b955b7dc5b3cf6d3f3396c84d42885ea6616f
Viet Nguyen <alexvn.edu@gmail.com>
2024-05-06 15:50:40 -0400

    running all algorithms on all benchmarks!

--- run_comparison.py
+++ run_comparison.py
@@ -1,70 +1,78 @@
+# %%
+
 import numpy as np
 from typing import Dict, List
 
 from lib.config import Config
 from lib.algorithms import FireFly, Bat, PSO, SlimeMould, SimulatedAnnealingSlimeMould
 from lib.benchmarks.functions import rosenbrock
+import lib.benchmarks.functions as function_module
+from inspect import getmembers, isfunction
 from lib.solve import compare
 
-FUNCT = rosenbrock
+# FUNCT = rosenbrock
 
-# Global variables (experiment setup)
-SEED = 1990
-TRIALS = 30
-GLOBAL = {
-    'D': 2,
-    'lb': -2.0,
-    'ub': 2.0,
-    'MAX_I': 1000,
-    'funct': FUNCT,
-    'min' :"Minimization",
-    'minimizing': FUNCT.minimizing,
-    'stop_criterion': {
-        'type': 'complex',
-        'criteria': [
-            {'type': 'iterations', 'max_iterations': 1000},
-            {'type': 'fitness', 'target_fitness': 1e-6}
-        ]
+# Loop through all the benchmark
+for (func_name, FUNCT) in getmembers(function_module, isfunction):
+
+    # Global variables (experiment setup)
+    SEED = 1990
+    TRIALS = 30
+    GLOBAL = {
+        'D': 2,
+        'lb': -2.0,
+        'ub': 2.0,
+        'MAX_I': 1000,
+        'funct': FUNCT,
+        'min' :"Minimization",
+        'minimizing': FUNCT.minimizing,
+        'stop_criterion': {
+            'type': 'complex',
+            'criteria': [
+                {'type': 'iterations', 'max_iterations': 1000},
+                {'type': 'fitness', 'target_fitness': 1e-6}
+            ]
+        }
     }
-}
 
-np.random.seed(SEED)
+    np.random.seed(SEED)
 
-populations = [5, 10, 25, 50, 100]
+    populations = [5, 10, 25, 50, 100]
 
-firefly_config = Config(gamma=1.0, alpha=0.5, beta0=1.0)
-firefly_config = firefly_config.update(GLOBAL)
+    firefly_config = Config(gamma=1.0, alpha=0.5, beta0=1.0)
+    firefly_config = firefly_config.update(GLOBAL)
 
-bat_config = Config(
-    alpha=1.0, 
-    pulse_rate=0.2, 
-    gamma=0.9,
-    freq_min=0,
-    freq_max=2,
-    loudness=0.25,
+    bat_config = Config(
+        alpha=1.0, 
+        pulse_rate=0.2, 
+        gamma=0.9,
+        freq_min=0,
+        freq_max=2,
+        loudness=0.25,
+        )
+    bat_config = bat_config.update(GLOBAL)
+
+    pso_config = Config(
+        alpha=0.9, 
+        beta=0.5,
+        v_max=1.0,  # Maximum velocity
+        v_init=0.1,  # Initial max velocity
+        )
+    pso_config = pso_config.update(GLOBAL)
+
+    slime_mould_config = Config(
+        max_iters=1000
     )
-bat_config = bat_config.update(GLOBAL)
+    slime_mould_config = slime_mould_config.update(GLOBAL)
 
-pso_config = Config(
-    alpha=0.9, 
-    beta=0.5,
-    v_max=1.0,  # Maximum velocity
-    v_init=0.1,  # Initial max velocity
+    simulated_annealing_slime_mould_config = Config(
+        max_iters=1000,
+        cooling_rate=0.8
     )
-pso_config = pso_config.update(GLOBAL)
+    simulated_annealing_slime_mould_config = simulated_annealing_slime_mould_config.update(GLOBAL)
 
-slime_mould_config = Config(
-    max_iters=1000
-)
-slime_mould_config = slime_mould_config.update(GLOBAL)
+    configs = [firefly_config, bat_config, pso_config, slime_mould_config, simulated_annealing_slime_mould_config]
+    algos = [FireFly, Bat, PSO, SlimeMould, SimulatedAnnealingSlimeMould]
 
-simulated_annealing_slime_mould_config = Config(
-    max_iters=1000,
-    cooling_rate=0.8
-)
-simulated_annealing_slime_mould_config = simulated_annealing_slime_mould_config.update(GLOBAL)
+    compare(algos, configs, populations, TRIALS, f"{func_name}")
 
-configs = [firefly_config, bat_config, pso_config, slime_mould_config, simulated_annealing_slime_mould_config]
-algos = [FireFly, Bat, PSO, SlimeMould, SimulatedAnnealingSlimeMould]
-
-compare(algos, configs, populations, TRIALS, "Test_Compare")
